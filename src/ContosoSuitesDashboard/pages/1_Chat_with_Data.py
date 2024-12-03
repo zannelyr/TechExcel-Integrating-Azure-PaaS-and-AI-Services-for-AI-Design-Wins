33170aa686e13dcb5bbbcd1999be689a87430245
Zannely <88408983+zannelyr@users.noreply.github.com>
2024-12-03 21:45:07 +0000

    adding updates

--- src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
+++ src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
@@ -7,6 +7,11 @@ def create_chat_completion(messages):
     """Create and return a new chat completion request. Key assumptions:
     - The Azure OpenAI endpoint, key, and deployment name are stored in Streamlit secrets."""
 
+    search_endpoint = st.secrets["search"]["endpoint"]
+    search_key = st.secrets["search"]["key"]
+    search_index_name = st.secrets["search"]["index_name"]
+
+
     # Retrieve secrets from the Streamlit secret store.
     # This is a secure way to store sensitive information that you don't want to expose in your code.
     # Learn more about Streamlit secrets here: https://docs.streamlit.io/develop/concepts/connections/secrets-management
@@ -27,8 +32,24 @@ def create_chat_completion(messages):
             {"role": m["role"], "content": m["content"]}
             for m in messages
         ],
-        stream=True
+        stream=True,
+        extra_body={
+            "data_sources": [
+                {
+                    "type": "azure_search",
+                    "parameters": {
+                        "endpoint": search_endpoint,
+                        "index_name": search_index_name,
+                        "authentication": {
+                            "type": "api_key",
+                            "key": search_key
+                        }
+                    }
+                }
+            ]
+        }
     )
+
 
 def handle_chat_prompt(prompt):
     """Echo the user's prompt to the chat window.
